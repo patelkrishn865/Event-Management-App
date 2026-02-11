1513204d459692c0a628e6b9ea81cb869302c06f
Krishn <22amtics070@gmail.com>
2026-02-11 10:11:04 +0530

    feat: redirect root URL to login page

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/auth/login",
+        permanent: true,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
